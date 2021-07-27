c0cf0adc0298d0a69f931f2ccd738d5092a876a3
Maxwell G. Brown <maxwellgbrown@gmail.com>
2021-07-27 12:45:50 -0400

    Use generic views

--- polls/views.py
+++ polls/views.py
@@ -2,30 +2,31 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 
 
-def index(request):
-    """Return index page."""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    """View to list the polls."""
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return queryset."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    """Return details associated with a poll."""
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    """View to see poll details."""
+    template_name = "polls/detail.html"
+    model = Question
 
 
-def results(request, question_id):
-    """Return results of a poll question."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    """View to see poll results."""
+    template_name = "polls/results.html"
+    model = Question
 
 
 def vote(request, question_id):
@@ -42,4 +43,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    # return HttpResponse(f"You're voting on question {question_id}")
